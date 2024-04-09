8fc97d019bc608a57679e356ca1213c2891f8a3d
rufusin <_rufusin_@mail.ru>
2024-04-09 14:13:34 +0300

    feat(YouTube - Tuck away preferences): Optimization default options of patch

--- src/main/kotlin/app/revanced/patches/youtube/layout/settings/HideSettingsPrefsPatch.kt
+++ src/main/kotlin/app/revanced/patches/youtube/layout/settings/HideSettingsPrefsPatch.kt
@@ -17,10 +17,8 @@ import org.w3c.dom.Element
 @Suppress("unused")
 object HideSettingsPrefsPatch : ResourcePatch() {
 
-    private const val DEFAULT_ELEMENTS = "Watch on TV, " +
-        "Manage all history, Privacy, Try experimental new features, " +
-        "Billing and payments, Notifications, Captions, Connected apps, " +
-        "Live chat, Accessibility"
+    private const val DEFAULT_ELEMENTS = "Watch on TV, Manage all history, Privacy, Try experimental new features, " +
+        "Purchases and memberships, Billing and payments, Captions, Connected apps, Accessibility"
 
     private val SettingElements by stringPatchOption(
         key = "SettingElements",
